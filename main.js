742d10eda0c88553902425630a47989452fe06ea
andreamazza84 <andrea.mazza84@gmail.com>
2020-11-23 17:15:29 +0100

    Pare funzionare

--- main.js
+++ main.js
@@ -7,26 +7,65 @@
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
-/** Restituisce 16 numeri casuali e diversi tra loro compresi tra 1 e 100
+
+/** Controlla che i numeri presenti nella lista siano tutti diversi tra loro
  *
  */
-function random100(){
-  var list = [];
+function checkDoubles(num, list) {
+  for (var index = 0; index < list.length; index++) {
+    if (num === list[index]) { return true; }
+  }
+  return false;
+}
+
+
+/** Restituisce 16 numeri casuali e diversi tra loro compresi tra 1 e 100
+*
+*/
+var randomList = [];
+function random16(){
   //Genera 16 numeri casuali
   for (var i = 0; i < 16; i++) {
     var num = Math.floor(Math.random()*100) + 1; //numeri casuali da 1 a 100
-    for (var index = 0; index < list.length; index++) {
-      if (num === list[index] && num < 100) {
-          list[index] = num + 1;
+    for (var index = 0; index < randomList.length; index++) {
+      if (num === randomList[index] && num < 100) {
+          randomList[index] = num + 1;
           index--; //doppio check
       }
-      else if (num === list[index] && num === 100) {
-          list[index] = num - 3;
+      else if (num === randomList[index] && num === 100) {
+          randomList[index] = num - 3;
           index--; //doppio check
       }
     }
-    list[i] = num;
+    randomList[i] = num;
   }
-  return list;
+  return randomList;
 }
-  alert("I sedici numeri casuali sono: " + random100());
+  //alert("I sedici numeri casuali sono: " + random100());
+
+
+// ************************************************************************** //
+
+var i, userNumber = 0;
+var userList = [];
+random16();
+
+for (var i = 0; i < 10; i++) {
+  userNumber = Number(prompt("Inserisci un numero da 1 a 100"));
+  
+  if (checkDoubles(userNumber, randomList)) {
+    alert("Hai perso");
+    break
+  }
+  if (i > 0 && checkDoubles(userNumber, userList)) {
+    alert("Hai perso");
+    break
+  }
+  userList[i] = userNumber;
+}
+
+console.log("userNumber " + userNumber);
+console.log("userList " + userList);
+console.log("randomList " + randomList);
+console.log("checkDoubles(userNumber, userList) " + checkDoubles(userNumber, userList));
+console.log("checkDoubles(userNumber, randomList) " + checkDoubles(userNumber, randomList));
